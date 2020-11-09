f0b518d35520c5545224fcb0ffc9c9e4e41e5385
Juan M.vi <earth001@gmail.com>
2020-11-09 07:38:44 -0300

    First commit

---
+++ tests/Unit/EmailTest.php
@@ -0,0 +1,36 @@
+<?php declare(strict_types=1);
+
+namespace Tests\Feature;
+
+use App\Examples\Email;
+use PHPUnit\Framework\TestCase;
+use InvalidArgumentException;
+
+final class EmailTest extends TestCase
+{
+    public function testCanBeCreatedFromValidEmailAddress(): void
+    {
+        $this->assertInstanceOf(
+            Email::class,
+            Email::fromString('user@example.com')
+        );
+    }
+
+    public function testCannotBeCreatedFromInvalidEmailAddress(): void
+    {
+        $this->expectException(InvalidArgumentException::class);
+
+        Email::fromString('invalid');
+    }
+
+    /**
+     * @test
+     */
+    public function CanBeUsedAsString(): void
+    {
+        $this->assertEquals(
+            'user@example.com',
+            Email::fromString('user@example.com')
+        );
+    }
+}
---
+++ tests/Unit/ExampleTest.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Tests\Unit;
+
+use PHPUnit\Framework\TestCase;
+
+class ExampleTest extends TestCase
+{
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function testBasicTest()
+    {
+        echo "this is a test";
+        $this->assertTrue(true);
+    }
+
+    public function sdasdTest()
+    {
+        echo "this is a test";
+        $this->assertTrue(true);
+    }
+}
