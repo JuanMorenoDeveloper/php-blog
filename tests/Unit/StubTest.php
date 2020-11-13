32706c94fcd80510122e080fa5060f9b8e63825b
Juan M.vi <earth001@gmail.com>
2020-11-12 21:26:07 -0300

    FIX Error in test

--- tests/Unit/StubTest.php
+++ tests/Unit/StubTest.php
@@ -18,6 +18,6 @@ final class StubTest extends TestCase
 
         // Calling $stub->doSomething() will now return
         // 'foo'.
-        $this->assertSame('foox', $stub->doSomething());
+        $this->assertSame('foo', $stub->doSomething());
     }
 }
